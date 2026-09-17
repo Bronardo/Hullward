7723d6f47dde789773bc49e7f5b94fae748c353c
leoshi <skyer4@icloud.com>
2026-09-17 13:59:02 +1000

    chore: add run_game.bat playtest launcher

---
+++ run_game.bat
@@ -0,0 +1,4 @@
+@echo off
+rem Hullward playtest launcher - double-click to run the game
+cd /d "%~dp0"
+start "" "D:\pg\Godot_v4.7.2-stable_mono_win64\Godot_v4.7.2-stable_mono_win64.exe" --path "%~dp0Hullward"
